e5babd9598b83ab1acc25f61c089d7d492e58bff
JhonValle <104865971+JhonValle@users.noreply.github.com>
2022-05-05 00:03:42 -0500

    Initial commit

---
+++ app/src/main/java/com/example/splash/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+    public static final long SPLASHP_SCREEN_DELAY=3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //IMPLEMENTAR TIEMPO
+        TimerTask time=new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent=new Intent().setClass(MainActivity.this,Splash1.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+        //Se ejecuta una vez cerrado
+        Timer timer=new Timer();
+        timer.schedule(time,SPLASHP_SCREEN_DELAY);
+
+    }
+}
---
+++ app/src/main/java/com/example/splash/Splash2.java
@@ -0,0 +1,33 @@
+package com.example.splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Splash2 extends AppCompatActivity {
+    public static final long SPLASH2_SCREEN_DELAY=3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash2);
+
+        //IMPLEMENTAR TIEMPO
+        TimerTask time=new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent=new Intent().setClass(Splash2.this,Splash1.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+        //Se ejecuta una vez cerrado
+        Timer timer=new Timer();
+        timer.schedule(time,SPLASH2_SCREEN_DELAY);
+
+    }
+}
